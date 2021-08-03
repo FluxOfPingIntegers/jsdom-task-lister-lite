11dfccb519f3d5d270d636e13c4ed1037b585ba5
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-03 12:33:16 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.getElementsByTagName('form')[0];
+  taskForm.addEventListener("submit", function(event) {
+    let userText = document.getElementById("new-task-description").value;
+    document.getElementById("tasks").innerHTML += `<li>${userText}</li>`;
+    console.log(`${userText}`);
+    event.preventDefault();
+  }, false);
+
+
 });
